c2924805e76bb88ee4596371a30a5a35c544ea9a
KingBard77 <badamin16@gmail.com>
2022-07-05 12:14:21 +0800

    init

---
+++ src/components/portfolio/Buttons.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Data from "./Data";
+
+const Buttons = ({ filterItem, setItem, menuItems }) => {
+    return ( <
+        >
+        <
+        div className = "portfolio__filters" >
+        <
+        button className = "portfolio__tab"
+        onClick = {
+            () => setItem(Data) } >
+        All <
+        /button> {
+            menuItems.map((Val, id) => {
+                return ( <
+                    button className = "portfolio__tab"
+                    onClick = {
+                        () => filterItem(Val) }
+                    key = { id } >
+                    { Val } <
+                    /button>
+                );
+            })
+        }
+
+        <
+        /div> <
+        />
+    );
+};
+
+export default Buttons;
